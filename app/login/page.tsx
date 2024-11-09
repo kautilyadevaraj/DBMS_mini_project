0c77aebce8e62349f03a7c2bf4ec06209be234d3
Kautilya DK <kautilyadk@gmail.com>
2024-11-09 07:44:57 +0530

    added login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -17,35 +17,29 @@ export default async function Page() {
     <div>
       <div className="container relative hidden h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
         <Link
-          href="/examples/authentication"
+          href="/login"
           className={cn(
             buttonVariants({ variant: "ghost" }),
             "absolute right-4 top-4 md:right-8 md:top-8"
           )}
-        >
-          Login
-        </Link>
+        ></Link>
         <div className="relative hidden h-full flex-col justify-center items-center bg-muted p-10 text-white dark:border-r lg:flex">
           <div className="absolute inset-0 bg-zinc-900" />
           <div className="relative z-20 flex flex-col justify-center items-center text-lg font-medium">
             <Image src={Logo} width={400} height={400} alt="logo" />
-            <div>
-              
-            </div>
           </div>
         </div>
         <div className="lg:p-8">
           <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
             <div className="flex flex-col space-y-2 text-center">
               <h1 className="text-2xl font-semibold tracking-tight">
-                Create an account
+                Login to your account
               </h1>
               <p className="text-sm text-muted-foreground">
-                Enter your email below to create your account
+                Enter your email below to login your account
               </p>
             </div>
-            <UserAuthForm />
-
+            <UserAuthForm />.
           </div>
         </div>
       </div>
